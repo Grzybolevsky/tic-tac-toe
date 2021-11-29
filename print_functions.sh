7e24080c862efd46670e0ba62aa0d47fcebfae77
Szymon Zak <szymon436@gmail.com>
2021-11-29 13:59:38 +0100

    Refactor

---
+++ print_functions.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function print_menu {
+    clear
+    echo "Menu:"
+    echo "1. New game"
+    echo "2. Load game"
+    printf "Enter: "
+};
+
+function print_game_type {
+    clear
+    echo "Game types:"
+    echo "1. Play with computer"
+    echo "2. Two players"
+    printf "Enter: "
+};
